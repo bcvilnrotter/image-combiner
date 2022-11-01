018b8c398ba82780cf10559bbe490a0e2812b6b2
bcvilnrotter <35104906+bcvilnrotter@users.noreply.github.com>
2022-10-31 23:17:58 -0400

    Update tabletop-prototype.py
    Hack-sawed code pieced together without being tested.
    
    Wanted to put plop something down here so I can test it out next session.
    
    I'm preparing for a lot of bugs...

--- tabletop-prototype.py
+++ tabletop-prototype.py
@@ -20,14 +20,14 @@ parser = argparse.ArgumentParser(formatter_class = argparse.RawDescriptionHelpFo
 parser.add_argument("-d", --directory, metavar="DIRECTORY_IMAGE", help="path to directory containing multiple base images")
 parser.add_argument("-i", --image, metavar="FILE_IMAGE", help="Path to our base image")
 parser.add_argument("-o", --output, metavar="OUTPUT_IMAGE", help="Path to directory where output will be placed")
-parser.add_argument("-n", --number, metavar="NUMBER", default=60, help = "Number of cards to be made in the deck")
+# parser.add_argument("-n", --number, metavar="NUMBER", default=60, help = "Number of cards to be made in the deck")
 
 # parse out the arguments
 args = vars(parser.parse_args())
 
 """
 This section of the code is solely focused on taking pictures from the user, and 
-combing them into a bigger picture that can be used for making decks in
+combining them into a bigger picture that can be used for making decks in
 TableTop Simulator (TTS). Although most of the functionality for this script will
 be for use in TableTop Simulator, future additions may stride away from that
 focus.
@@ -36,30 +36,58 @@ author: Brian Vilnrotter
 """
 
 # function to build deck image
-def tts_builddeck(file, output, number):
+def tts_builddeck(file, output):
       
-      #<TODO>
-
-# check if "-i" arguement is called
-if args["image"]:
+      # import the user provided image
+      image = Image.open(file)
       
-      # make the card deck image
-      tts_builddeck(args["image"], args["output"], args["number"])
-
-# else, check if "-o" argument is called
-elif args["directory"]:
+      # get specs of uploaded image
+      width, height = image.size
       
-      # iterate recursively through the directory provided
-      for subdir, dirs, files in os.walk(args["directory"]):
+      # create the coeficient dimensions of the new image to be created
+      nwidth, nheight = [7, 10]
+      
+      # create new image variable
+      new = Image.new(file.mode, (nwidth*width, nheight*height))
+      
+      # iterate through the new images height
+      for h_index in range(nheight):
             
-            # with the created values iterate through the files
-            for file in files:
+            # iterate through the new images width
+            for w_index in range(nwidth):
                   
-                  # make a path of each file using created values
-                  path = os.path.join(subdir, file)
+                  # paste the image based on the index number
+                  new.paste(width*w_index, height*h_index)
                   
-                  # make the card deck image
-                  tts_builddeck(path, args["output"], args["number"])
+      # save created image to the designated output path
+      new.save(output, quality=95)
+
+# function __main__
+def main():
+
+      # check if "-i" arguement is called
+      if args["image"]:
+
+            # make the card deck image
+            tts_builddeck(args["image"], args["output"])
+
+      # else, check if "-o" argument is called
+      elif args["directory"]:
+
+            # iterate recursively through the directory provided
+            for subdir, dirs, files in os.walk(args["directory"]):
+
+                  # with the created values iterate through the files
+                  for file in files:
+
+                        # make a path of each file using created values
+                        path = os.path.join(subdir, file)
+
+                        # make the card deck image
+                        tts_builddeck(path, args["output"])
+
+if "__name__" = "__main__":
+      main()
 
 # directory = args["secondary"]
 # resources = []
