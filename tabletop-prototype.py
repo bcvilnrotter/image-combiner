9c3ef5e8acc00b194479b4838dfb10ecef4a163e
Brian Vilnrotter <bcvilnrotter@gmail.com>
2022-11-14 23:10:36 -0500

    syncing local repo with vs code

--- tabletop-prototype.py
+++ tabletop-prototype.py
@@ -2,8 +2,9 @@
 
 # imports
 import argparse
-import os, sys
+import os
 import random
+import sys
 from datetime import datetime, timezone
 from PIL import Image
 
@@ -34,13 +35,13 @@ tts_deckbuilder.add_argument("-i", '--image', help="Path to our base image")
 
 # initialize the subparser arguments for tts_mapbuilder
 tts_mapbuilder.add_argument("-a", '--assets', help="path to the directory containing multiple images that are assets for the map")
-tts_mapbuilder.add_argument("-i", '--image', help="path to the background image used for the map")
+tts_mapbuilder.add_argument("-b", '--background', help="path to the background image used for the map")
 
 # parser.add_argument("-o", '--output', metavar="OUTPUT_IMAGE", help="Path to directory where output will be placed")
 # parser.add_argument("-n", --number, metavar="NUMBER", default=60, help = "Number of cards to be made in the deck")
 
-# parse out the arguments
-args = parser.parse_args()
+# parse out the arguments for the tts_deckbuilder function
+args_tts_deckbuilder = parser.parse_args('tts_deckbuilder')
 
 """
 This section of code will be dedicated to error/log messaging. This section
@@ -148,7 +149,7 @@ Author: Brian Vilnrotter
 """
 
 # function for creating a map
-def tts_mapbuilder(background, folder, assets = [], resize = (100,100)):
+def tts_buildmap(background, folder, assets = [], resize = (100,100)):
 
 	# load the background image
 	image = Image.open(background)
@@ -175,7 +176,7 @@ def tts_mapbuilder(background, folder, assets = [], resize = (100,100)):
 		asset = assets[random.randint(0, len(assets) -1)]
 
 		# paste the chosen asset on the background in the chosen position
-		image.paste(asset, positions, asset)
+		image.paste(asset, position, asset)
 	
 	# save the created image out
 	image.save(outpath(background))
@@ -190,22 +191,22 @@ Author: Brian Vilnrotter
 def main():
 
 	# check if "-i" arguement is called
-	if args.image:
+	if args_tts_deckbuilder.image:
 
 		# log the action
-		log('INFO', 'Image path provided: ' + str(args.image))
+		log('INFO', 'Image path provided: ' + str(args_tts_deckbuilder.image))
 		
 		# make the card deck image
-		tts_builddeck(args.image, outpath(args.image))
+		tts_builddeck(args_tts_deckbuilder.image, outpath(args_tts_deckbuilder.tts_deckbuiler.image))
 
 	# else, check if "-d" argument is called
-	elif args.directory:
+	elif args_tts_deckbuilder.directory:
 
 		# log the action
-		log('INFO', 'Directory path provided: ' + str(args.directory))
+		log('INFO', 'Directory path provided: ' + str(args_tts_deckbuilder.directory))
 
 		# iterate recursively through the directory provided
-		for subdir, dirs, files in os.walk(args.directory):
+		for subdir, dirs, files in os.walk(args_tts_deckbuilder.directory):
 
 			# with the created values iterate through the files
 			for file in files:
@@ -214,7 +215,7 @@ def main():
 				path = os.path.join(subdir, file)
 				
 				# make the card deck image
-				tts_builddeck(path, outpath(os.path.join(args.directory, file)))
+				tts_builddeck(path, outpath(os.path.join(args_tts_deckbuilder.directory, file)))
 
 if __name__ == "__main__":
 	main()
